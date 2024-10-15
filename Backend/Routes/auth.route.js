77b6b0f10129371c8263fb8f47425731aa7791a9
Mundane-Editorial <uday2k17@rediffmail.com>
2024-10-15 20:13:42 +0530

    added further routes

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -2,8 +2,22 @@ import express from 'express';
 
 const router = express.Router();
 
+
+//signup auth route
 router.get("/signup", (req, res)=>{
     res.send("SignUp Router called");
+});
+
+
+//login auth route
+router.get("/login", (req, res)=>{
+    res.send("Login Router called");
+});
+
+
+//logout auth route
+router.get("/logout", (req, res)=>{
+    res.send("Log Out Router called");
 });
 
 export default router;
